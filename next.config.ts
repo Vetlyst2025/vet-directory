31e50be9ca03aa7efb4d6e4f4638926d2d62a2a3
Vetlyst2025 <practicemanager@healthypetvetclinic.com>
2025-10-16 14:58:16 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  experimental: {
-    allowedDevOrigins: ['smart-pots-tickle.lindy.site'],
+
   },
 };
 
